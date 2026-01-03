1e4b2412c3184f38a5b4c573aaf6238d7e57036b
mohammadmehdisharife <mohammadmehdisharife@gmail.com>
2026-01-03 10:37:31 +0000

    add --version --help

--- scaffold.c
+++ scaffold.c
@@ -16,11 +16,48 @@ void execute_command(char *command)
     system(command);
 }
 
+void print_version()
+{
+    printf("scaffold - Tiny Crass-Platform Build System\n");
+    printf("Version: 1.0.0\n");
+    printf("Author: mohammad mehdi sharife\n");
+    printf("License: GPLv3\n");
+    printf("GitHub: https://github.com/mohammadmehdisharife/scaffold\n");
+    exit(0);
+}
+
+void print_help()
+{
+    printf("Usage: scaffold <target> [options]\n");
+    printf("       scaffold --help\n");
+    printf("       scaffold --version\n\n");
+    printf("Options:\n");
+    printf("  <target>    Target to execute from scaffold file\n");
+    printf("  --help      Show this help message\n");
+    printf("  --version   Show version information\n\n");
+    printf("Examples:\n");
+    printf("  scaffold build\n");
+    printf("  scaffold clean\n");
+    printf("  scaffold --version\n");
+    exit(0);
+}
+
 int main(int argc, char *argv[])
 {
     if (argc < 2) {
         print_error("Usage: scaffold <target>");
     }
+
+    if (strcmp(argv[1], "--version") == 0) {
+        print_version();
+        exit(0);
+    }
+
+    if (strcmp(argv[1], "--help") == 0) {
+        print_help();
+        exit(0);
+    }
+
 
     char *target = argv[1];
     FILE *file = fopen("scaffold", "r");
